55db8b2b52e9b2a9c79135480b653b8f6e21c059
TbotaPhantA <botakiller@mail.ru>
2024-03-19 21:30:46 +0000

    minor changes

--- labrab27_33/main.go
+++ labrab27_33/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	// Task27()
 	// Task28_29() // go run . https://en.wikipedia.org/wiki/Ancient_Rome
-	Task30()
+	// Task30()
 	// Task31()
 	// Task32()
 	// Task33() // go run . --threads=5
